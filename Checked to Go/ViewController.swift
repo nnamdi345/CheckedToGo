e098c6a084bc2ba7760d56a2fe348f891b8d75c1
nnamdi345 <70900394+nnamdi345@users.noreply.github.com>
2021-06-30 11:55:32 -0400

    Initial Commit

---
+++ Checked to Go/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checked to Go
+//
+//  Created by Nnamdi Iroha on 6/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
